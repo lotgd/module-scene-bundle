ba5211cb438437fe4cce58f774355c2eae0a2320
Vassyli <basilius.sauter@gmail.com>
2017-03-03 19:13:02 +0100

    Updated module scene bundle to newest core api.

--- src/Module.php
+++ src/Module.php
@@ -6,6 +6,8 @@ namespace LotGD\Module\SceneBundle;
 use Doctrine\Common\Collections\ArrayCollection;
 
 use LotGD\Core\Game;
+use LotGD\Core\Models\SceneConnectionGroup;
+use LotGD\Core\Models\SceneConnectable;
 use LotGD\Core\Module as ModuleInterface;
 use LotGD\Core\Models\Module as ModuleModel;
 use LotGD\Core\Models\Scene;
@@ -18,39 +20,53 @@ class Module implements ModuleInterface {
         "lotgd/module-scene-bundle/pond/oak",
     ];
 
+    const Groups = [
+        "pond" => [
+            "lotgd/module-scene-bundle/pond/the-pond",
+            "lotgd/module-scene-bundle/pond/back"
+        ],
+        "oak" => [
+            "lotgd/module-scene-bundle/oak/back",
+        ]
+    ];
+
     public static function handleEvent(Game $g, string $event, array &$context)
     {
 
     }
 
-    private static function getBaseScene($template)
+    private static function getBaseScene(): array
     {
-        switch($template) {
-            case self::SceneTemplates[0]:
-                $pondScene = Scene::create([
-                    "template" => self::SceneTemplates[0],
-                    "title" => "The Pond",
-                    "description" => "A bit outside of the village, near the border to the dark deep forest, "
-                        . "there is a place simply called «The Pond». It's name-giver, a small pond with a beautiful, "
-                        . "blue-green colour, is in it's centre, enclosed by meadow. After seeing this place, everyone "
-                        . "nows immediatly why this place is liked among lovers. A bit more separate, an old oak tree"
-                        . "stands mighty.",
-                ]);
+        $pondScene = Scene::create([
+            "template" => self::SceneTemplates[0],
+            "title" => "The Pond",
+            "description" => "A bit outside of the village, near the border to the dark deep forest, "
+                . "there is a place simply called «The Pond». It's name-giver, a small pond with a beautiful, "
+                . "blue-green colour, is in it's centre, enclosed by meadow. After seeing this place, everyone "
+                . "nows immediatly why this place is liked among lovers. A bit more separate, an old oak tree"
+                . "stands mighty.",
+        ]);
 
-                $oakScene = Scene::create([
-                    "template" => self::SceneTemplates[1],
-                    "title" => "The old oak",
-                    "description" => "The old oaken tree, the oldest tree inside of the village and one of the few that "
-                        . "didn't fell victim to the woodcutter, has a lot of old and fresh hearts cut inside it's bork, "
-                    . "witness to the numerous lover's that wanted their love to last as long as this tree does.",
-                ]);
+        $pondScene->addConnectionGroup(new SceneConnectionGroup(self::Groups["pond"][0], "The Pond"));
+        $pondScene->addConnectionGroup(new SceneConnectionGroup(self::Groups["pond"][1], "Back"));
 
-                $oakScene->setParents(new ArrayCollection([$pondScene]));
+        $oakScene = Scene::create([
+            "template" => self::SceneTemplates[1],
+            "title" => "The old oak",
+            "description" => "The old oaken tree, the oldest tree inside of the village and one of the few that "
+                . "didn't fell victim to the woodcutter, has a lot of old and fresh hearts cut inside it's bork, "
+            . "witness to the numerous lover's that wanted their love to last as long as this tree does.",
+        ]);
 
-                return $pondScene;
-        }
+        $pondScene->addConnectionGroup(new SceneConnectionGroup(self::Groups["oak"][0], "Back"));
 
-        return null;
+        $pondScene
+            ->getConnectionGroup(self::Groups["pond"][0])
+            ->connect(
+                $oakScene->getConnectionGroup(self::Groups["oak"][0])
+            );
+
+        return [$pondScene, $oakScene];
     }
 
     public static function onRegister(Game $g, ModuleModel $module)
@@ -60,10 +76,21 @@ class Module implements ModuleInterface {
 
         foreach ($villageScenes as $villageScene) {
             foreach (self::SceneTemplates as $template) {
-                $scene = self::getBaseScene($template);
-                if ($scene !== null) {
-                    $scene->setParents(new ArrayCollection([$villageScene]));
+                [$pondScene, $oakScene] = self::getBaseScene();
+
+                // Connect the pond to the village
+                if ($villageScene->hasConnectionGroup(VillageModule::Group[1])) {
+                    $villageScene
+                        ->getConnectionGroup(VillageModule::Group[1])
+                        ->connect($pondScene->getConnectionGroup(self::Groups["pond"][1]));
+                } else {
+                    $villageScene->connect($pondScene->getConnectionGroup(self::Groups["pond"][1]));
                 }
+
+                // connect the oak to the village in one direction only.
+                $oakScene
+                    ->getConnectionGroup(self::Groups["oak"]["back"])
+                    ->connect($villageScene, SceneConnectable::Unidirectional);
             }
         }
 
